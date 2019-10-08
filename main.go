ec38d153ad2998970b6ce51e5778c2473fa2aed9
kifen <kifenraph@gmail.com>
2019-10-08 10:45:58 +0100

    updating webserver

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"github.com/Kifen/go-jwt/common"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	router := common.HandleRequests()
-	fmt.Println("listening...")
-	err := http.ListenAndServe(":3000", router)
+	port := os.Getenv("PORT")
+	log.Printf("Port is = %s",port)
+	if port == "" {
+		port = "3000"
+	}
+	fmt.Printf("Listening on port :%s", port)
+	err := http.ListenAndServe(":" + port, router)
 	if err !=nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
