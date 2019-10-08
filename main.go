7b7603e5942a5aa6ac3e859de3d8c7d60d01e208
kifen <kifenraph@gmail.com>
2019-10-08 10:16:56 +0100

    :port

--- main.go
+++ main.go
@@ -5,24 +5,14 @@ import (
 	"github.com/Kifen/go-jwt/common"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	router := common.HandleRequests()
 	fmt.Println("listening...")
-	err := http.ListenAndServe(GetPort(), router)
+	err := http.ListenAndServe(":3000", router)
 	if err !=nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func GetPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-	}
-
-	return ":" + port
-}
